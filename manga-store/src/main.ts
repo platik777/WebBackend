eb428739516ad1a21d52d0fd40404653521b1ad4
platik777 <ps7654400@icloud.com>
2025-09-17 18:23:14 +0300

    fix package.json

--- manga-store/src/main.ts
+++ manga-store/src/main.ts
@@ -213,8 +213,8 @@ async function bootstrap() {
     `,
   });
 
-  const port = configService.get<number>('PORT', 3000);
-  await app.listen(port);
+  const port = process.env.PORT || configService.get<number>('PORT', 3000);
+  await app.listen(port, '0.0.0.0');
 
   console.log(`Application is running on: http://localhost:${port}`);
   console.log(`API Documentation: http://localhost:${port}/api/docs`);
